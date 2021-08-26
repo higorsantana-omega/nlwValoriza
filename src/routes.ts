8a163c2fac90a93e1e295b56f513a3aee7541993
higorsantana-omega <70420430+higorsantana-omega@users.noreply.github.com>
2021-08-25 21:10:05 -0300

    configure eslint

--- src/routes.ts
+++ src/routes.ts
@@ -1,43 +1,60 @@
-import { Router } from "express"
-import { CreateUserController } from "./controllers/CreateUserController"
-import { CreateTagController } from "./controllers/CreateTagController"
-import { ensureAdmin } from "./middlewares/ensureAdmin"
-import { AuthenticateUserController } from "./controllers/AuthenticateUserController"
-import { CreateComplimentController } from "./controllers/CreateComplimentController"
-import { ensureAuthenticated } from "./middlewares/ensureAuthenticated"
-import { ListUserSendComplimentsController } from "./controllers/ListUserSendComplimentsController"
-import { ListUserReceiveComplimentsController } from "./controllers/ListUserReceiveComplimentsController"
-import { ListTagsController } from "./controllers/ListTagsController"
-import { ListUsersController } from "./controllers/ListUsersController"
+import { Router } from 'express';
+import { CreateUserController } from './controllers/CreateUserController';
+import { CreateTagController } from './controllers/CreateTagController';
+import { ensureAdmin } from './middlewares/ensureAdmin';
+import { AuthenticateUserController } from './controllers/AuthenticateUserController';
+import { CreateComplimentController } from './controllers/CreateComplimentController';
+import { ensureAuthenticated } from './middlewares/ensureAuthenticated';
+import { ListUserSendComplimentsController } from './controllers/ListUserSendComplimentsController';
+import { ListUserReceiveComplimentsController } from './controllers/ListUserReceiveComplimentsController';
+import { ListTagsController } from './controllers/ListTagsController';
+import { ListUsersController } from './controllers/ListUsersController';
 
-const router = Router()
+const router = Router();
 
-const createUserController = new CreateUserController()
-const createTagController = new CreateTagController()
-const authenticateUserController = new AuthenticateUserController()
-const createComplimentController = new CreateComplimentController()
-const listUserSendComplimentsController = new ListUserSendComplimentsController()
-const listUserReceiveComplimentsController = new ListUserReceiveComplimentsController()
+const createUserController = new CreateUserController();
+const createTagController = new CreateTagController();
+const authenticateUserController = new AuthenticateUserController();
+const createComplimentController = new CreateComplimentController();
+const listUserSendComplimentsController =
+  new ListUserSendComplimentsController();
+const listUserReceiveComplimentsController =
+  new ListUserReceiveComplimentsController();
 
-const listTagsController = new ListTagsController()
+const listTagsController = new ListTagsController();
 
-const listUsersController = new ListUsersController()
+const listUsersController = new ListUsersController();
 
-
-router.post("/tags", ensureAuthenticated, ensureAdmin, createTagController.handle)
+router.post(
+  '/tags',
+  ensureAuthenticated,
+  ensureAdmin,
+  createTagController.handle,
+);
 
 // Rota de usuario; instanciando o createUserController
-router.post("/users", createUserController.handle)
+router.post('/users', createUserController.handle);
 
-router.post("/login", authenticateUserController.handle)
-router.post("/compliments", ensureAuthenticated, createComplimentController.handle)
+router.post('/login', authenticateUserController.handle);
+router.post(
+  '/compliments',
+  ensureAuthenticated,
+  createComplimentController.handle,
+);
 
-router.get("/users/compliments/send", ensureAuthenticated, listUserSendComplimentsController.handle)
-router.get("/users/compliments/receive", ensureAuthenticated, listUserReceiveComplimentsController.handle)
+router.get(
+  '/users/compliments/send',
+  ensureAuthenticated,
+  listUserSendComplimentsController.handle,
+);
+router.get(
+  '/users/compliments/receive',
+  ensureAuthenticated,
+  listUserReceiveComplimentsController.handle,
+);
 
-router.get("/tags", ensureAuthenticated, listTagsController.handle)
+router.get('/tags', ensureAuthenticated, listTagsController.handle);
 
-router.get("/users", ensureAuthenticated, listUsersController.handle)
+router.get('/users', ensureAuthenticated, listUsersController.handle);
 
-
-export { router }
+export { router };
